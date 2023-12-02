ef5ac29cadf09da13c751c61666487239ae031f6
Mahesh-100 <maheshkumar.ucen@gmail.com>
2023-12-02 12:25:29 +0530

    react

--- src/index.js
+++ src/index.js
@@ -5,3 +5,9 @@ var h=React.createElement("h1",{},"hello world by react")
 var res=document.getElementById("root")
 var result=ReactDOM.createRoot(res)
 result.render(h)
+//nested element
+var x=React.createElement("h1",{},"THIS IS HEADING")
+var y=React.createElement("p",{},"nested element")
+var z=document.getElementById("root1")
+var r=ReactDOM.createRoot(z)
+r.render(React.createElement("div",{},x,y));
